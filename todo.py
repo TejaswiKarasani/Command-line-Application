89c76416b9ac17c5f36d3f306ee78a6339ce6e57
TejaswiKarasani <tejaswikarasani@gmail.com>
2020-12-21 22:16:47 +0530

    initial setup

---
+++ todo.py
@@ -0,0 +1,7 @@
+print('Usage :-')
+print('$ ./todo add "todo item"   # Add a new todo')
+print('$ ./todo ls                # Show remaining todos')
+print('$ ./todo del NUMBER        # Delete a todo')
+print('$ ./todo done NUMBER       # Complete a todo')
+print('$ ./todo help              # Show usage')
+print('$ ./todo report            # Statistics')
